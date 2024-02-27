ddc26ec21a54beac308a447a9d8edd86cc99c55f
Saravanan G <saravanang@syncfusion.com>
2024-02-27 18:03:07 +0530

    Added the demo files
    Added the demo files

---
+++ BlazorDataFormLocalization/BlazorDataFormLocalization.Client/Data/UserDetails.cs
@@ -0,0 +1,38 @@
+﻿using BlazorDataFormLocalization.Client.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDataFormLocalization.Data
+{
+    public class UserDetails
+    {
+        [Display(ResourceType = typeof(SfResources), Name = "DataForm_FirstName")]
+        [Required(ErrorMessageResourceName = "DataForm_FirstName_Error", ErrorMessageResourceType = typeof(SfResources))]
+        public string FirstName { get; set; }
+
+        [Display(ResourceType = typeof(SfResources), Name = "DataForm_LastName")]
+        [Required(ErrorMessageResourceName = "DataForm_LastName_Error", ErrorMessageResourceType = typeof(SfResources))]
+        public string LastName { get; set; }
+
+        [Display(ResourceType = typeof(SfResources), Name = "DataForm_Email")]
+        [Required(ErrorMessageResourceName = "DataForm_Email_Error", ErrorMessageResourceType = typeof(SfResources))]
+        [EmailAddress(ErrorMessageResourceName = "DataForm_Valid_Email_Error", ErrorMessageResourceType = typeof(SfResources))]
+        public string Email { get; set; }
+
+        [Display(ResourceType = typeof(SfResources), Name = "DataForm_Password")]
+        [MinLength(8, ErrorMessageResourceName = "DataForm_Password_Length_Error", ErrorMessageResourceType = typeof(SfResources))]
+        [Required(ErrorMessageResourceName = "DataForm_Password_Error", ErrorMessageResourceType = typeof(SfResources))]
+        public string Password { get; set; }
+
+        [Display(ResourceType = typeof(SfResources), Name = "DaaForm_DateOfBirth")]
+        [Required(ErrorMessageResourceName = "DataForm_DateOfBirth_Error", ErrorMessageResourceType = typeof(SfResources))]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessageResourceName = "DataForm_AcceptTerms_Error", ErrorMessageResourceType = typeof(SfResources))]
+        [Display(ResourceType = typeof(SfResources), Name = "DataForm_AcceptTerms")]
+        public bool AcceptTerms { get; set; }
+
+        public UserDetails()
+        {
+        }
+    }
+}
---
+++ BlazorDataFormLocalization/BlazorDataFormLocalization.Client/SyncfusionLocalizer.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.Blazor;
+namespace BlazorDataFormLocalization.Client
+{
+    public class SyncfusionLocalizer : ISyncfusionStringLocalizer
+    {
+        public string GetText(string key)
+        {
+            return this.ResourceManager.GetString(key);
+        }
+
+        public System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                // Replace the ApplicationNamespace with your application name.
+                return BlazorDataFormLocalization.Client.Resources.SfResources.ResourceManager;
+            }
+        }
+    }
+}
